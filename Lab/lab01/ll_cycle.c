7d4ab8b2b7de167a316bfcae64e5490c4ec305a1
root <root@LAPTOP-E6LOJUP7.localdomain>
2025-09-18 09:04:57 +0800

    lab01: finish

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL || head->next == NULL) {
+        return 0;
+    }
+    
+    node* tortoise = head;
+    node* hare = head->next;
+
+    while (hare != NULL && hare->next != NULL) {
+        tortoise = tortoise->next->next;
+        hare = hare->next;
+
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
+
     return 0;
 }
